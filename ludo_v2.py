31d045ce0c69290a3283020e2df1b4a5c201e23f
Shadrack Agyei Nti <116956047+ShadrackAgyei@users.noreply.github.com>
2024-03-31 16:03:59 +0000

    Add files via upload

---
+++ ludo_v2.py
@@ -0,0 +1,15 @@
+# Ludo game Version 2 By group 8, No visuals included
+# Members:
+# Shadrack Agyei Nti
+# David Kweku Amissah Orhin
+# Boss Baeta
+# Edudzi Ama Nyaho
+# Jessica Doesi Tetteh-Yumu
+
+
+from main_ludo2 import *
+
+
+game_logic = SimpleLudoGame()
+game_interface = LudoGameInterface()
+game_interface.play_game(game_logic)
